0bb8485fedebc7f7e54869b041d62d937ba5106c
NeveenBeiram <neveenbeiram@yahoo.com>
2021-05-27 02:29:51 +0300

    ....

--- __tests__/auth.router.test.js
+++ __tests__/auth.router.test.js
@@ -1,82 +1,61 @@
 'use strict';
+require('dotenv').config();
 
-process.env.SECRET = 'toes';
+const server = require('../src/server.js').server;
 
-const server = require('../src/server').server;
 const supergoose = require('@code-fellows/supergoose');
-const bearer = require('../src/auth/middleware/bearer');
 
-const mockRequest = supergoose(server);
+const fakeServer = supergoose(server);
 
-let users = {
-  admin: { username: 'admin', password: 'password' },
-  editor: { username: 'editor', password: 'password' },
-  user: { username: 'user', password: 'password' },
+let user = {
+  username: 'fakeUser',
+  password: 'admin',
+  role: 'admin',
 };
 
-describe('Auth Router', () => {
 
-  Object.keys(users).forEach(userType => {
 
-    describe(`${userType} users`, () => {
+describe('Authentication Routes', () => {
+  it('POST /signup creates a new user and sends an object with the user and the token to the client', async() => {
+    let test = await fakeServer.post('/signup').send(user);
 
-      it('can create one', async () => {
-
-        const response = await mockRequest.post('/signup').send(users[userType]);
-        const userObject = response.body;
-
-        expect(response.status).toBe(201);
-        expect(userObject.token).toBeDefined();
-        expect(userObject.user._id).toBeDefined();
-        expect(userObject.user.username).toEqual(users[userType].username);
-
-      });
-
-      it('can signin with basic', async () => {
-
-        const response = await mockRequest.post('/signin')
-          .auth(users[userType].username, users[userType].password);
-
-        const userObject = response.body;
-        expect(response.status).toBe(200);
-        expect(userObject.token).toBeDefined();
-        expect(userObject.user._id).toBeDefined();
-        expect(userObject.user.username).toEqual(users[userType].username);
-
-      });
-
-    
-
-    });
-
-    describe('bad logins', () => {
-      it('basic fails with known user and wrong password ', async () => {
-
-        const response = await mockRequest.post('/signin')
-          .auth('admin', 'xyz');
-        const userObject = response.body;
-
-        expect(response.status).toBe(403);
-        expect(userObject.user).not.toBeDefined();
-        expect(userObject.token).not.toBeDefined();
-
-      });
-
-      it('basic fails with unknown user', async () => {
-
-        const response = await mockRequest.post('/signin')
-          .auth('nobody', 'xyz');
-        const userObject = response.body;
-
-        expect(response.status).toBe(403);
-        expect(userObject.user).not.toBeDefined();
-        expect(userObject.token).not.toBeDefined();
-
-      });
-
-      
-    });
-
+    expect(test.body.user.username).toEqual('fakeUser');
+    expect(test.body.token).toBeDefined();
+    expect(test.status).toEqual(201);
   });
 
+  it('POST /signin with basic authentication headers logs in a user and sends an object with the user and the token to the client', async() => {
+
+    let test = await fakeServer.post('/signin').auth(user.username, user.password);
+
+
+    expect(test.body.user.role).toEqual('admin');
+    expect(test.status).toEqual(200);
+  });
+
+
+  it('GET /users with bearer token and permission to delete as an admin', async() => {
+    let testS = await fakeServer.post('/signin').auth(user.username, user.password);
+
+    let token = ` Bearer ${testS.body.token}`;
+
+    let test = await fakeServer.get('/users').set(`Authorization`, `${token}`);
+
+
+    expect(test.body[0]).toEqual('fakeUser');
+    expect(test.status).toEqual(200);
+  });
+
+
+  it('GET /secret with bearer token', async() => {
+    let testS = await fakeServer.post('/signin').auth(user.username, user.password);
+
+    let token = `Bearer ${testS.body.token}`;
+
+    let test = await fakeServer.get('/secret').set(`Authorization`, `${token}`);
+
+
+    expect(test.text).toEqual('Welcome to the secret area');
+    expect(test.status).toEqual(200);
+  });
 });
